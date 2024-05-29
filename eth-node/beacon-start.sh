c06d0e5f5a2f6f09a796ed0984a9d4ed15fa9417
mark <mark@gmail.com>
2024-05-29 12:29:19 +0800

    update eth node

--- eth-node/beacon-start.sh
+++ eth-node/beacon-start.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 /usr/local/bin/beacon-chain generate-auth-secret
+# Sleep for 10 seconds to allow eth service to fully start
+sleep 30
 
 /usr/local/bin/beacon-chain \
     --accept-terms-of-use \
-    --execution-endpoint=http://eth:22314 \
+    --execution-endpoint=http://eth:8551 \
     --jwt-secret=/data/prysm/jwt.hex \
-    --checkpoint-sync-url=https://beaconstate.info \
-    --genesis-beacon-api-url=https://beaconstate.info \
-    --datadir /data/beacondata
+    --checkpoint-sync-url=https://sync.invis.tools \
+    --genesis-beacon-api-url=https://sync.invis.tools \
+    --datadir /data/beacondata \
+    --rpc-host=0.0.0.0 \
+    --grpc-gateway-host=0.0.0.0
